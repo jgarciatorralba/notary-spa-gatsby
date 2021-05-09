e66da73d2d52fdd98c7b7ba121eb60444b94ad06
jgarciatorralba <jgarciatorralba@gmail.com>
2021-05-09 13:24:36 +0200

    remove padding right in language items

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -92,7 +92,7 @@ const Navbar = () => {
                   </div>
                 </div>
               </div>
-              <ul className="languages absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <ul className="languages absolute inset-y-0 right-0 flex items-center sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {/* <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
