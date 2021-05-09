ed3d80ddad7d569ed5ea4fa60c3f520bdc37e7ca
jgarciatorralba <jgarciatorralba@gmail.com>
2021-05-09 12:20:34 +0200

    update gatsby-plugin-manifest and clear comments

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Notaría Mínguez`,
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque blandit egestas. Etiam ac elit a erat dictum imperdiet. In enim nunc, fringilla sit amet dictum non, tempor vel augue. Integer scelerisque sem varius fermentum tincidunt. Mauris sagittis dignissim tortor vel dictum. Nulla eu condimentum leo. Fusce rhoncus nibh mi, vitae ultrices purus mollis a. Maecenas pharetra posuere nibh, id vehicula ligula ultrices id.`,
+    description: `ES: Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque blandit egestas. Etiam ac elit a erat dictum imperdiet. In enim nunc, fringilla sit amet dictum non, tempor vel augue. Integer scelerisque sem varius fermentum tincidunt. Mauris sagittis dignissim tortor vel dictum. Nulla eu condimentum leo. Fusce rhoncus nibh mi, vitae ultrices purus mollis a. Maecenas pharetra posuere nibh, id vehicula ligula ultrices id.`,
     author: `@jgarciatorralba`,
   },
   plugins: [
@@ -19,13 +19,22 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Notaría Mínguez en Vic`,
+        short_name: `Notaría Mínguez`,
+        lang: `es`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#fff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/demo/emblem.png`,
+        localize: [
+          {
+            start_url: `/ca/`,
+            lang: `ca`,
+            name: `Notaria Mínguez a Vic`,
+            short_name: `Notaría Mínguez`,
+          },
+        ],
       },
     },
     `gatsby-plugin-gatsby-cloud`,
@@ -53,26 +62,14 @@ module.exports = {
         defaultLanguage: `es`,
         // if you are using Helmet, you must include siteUrl, and make sure you add http:https
         siteUrl: `http://localhost:8000/`,
-        // you can pass any i18next options
         // pass following options to allow message content as a key
         i18nextOptions: {
           interpolation: {
-            escapeValue: false, // not needed for react as it escapes by default
+            escapeValue: false,
           },
           keySeparator: false,
           nsSeparator: false,
         },
-        pages: [
-          {
-            matchPath: "/:lang?/blog/:uid",
-            getLanguageFromPath: true,
-            excludeLanguages: ["es"],
-          },
-          {
-            matchPath: "/preview",
-            languages: ["en"],
-          },
-        ],
       },
     },
   ],
