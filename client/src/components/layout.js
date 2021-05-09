e8797a14a6e879e37e67ca7ec3839ba0ebe41fa7
jgarciatorralba <jgarciatorralba@gmail.com>
2021-05-09 20:28:57 +0200

    remove unused graphql query in layout component

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -1,23 +1,10 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Navbar from "./navbar"
 import Footer from "./footer"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-
   return (
     <>
       <Navbar />
