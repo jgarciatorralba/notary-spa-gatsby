1756c99b4846cd07f3eb9ce707a94a8c6d2e29fc
Jorge García <jorge@omada.es>
2021-05-20 10:03:52 +0200

    add complete color palette to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,38 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ["./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      blueGray: colors.blueGray,
+      coolGray: colors.coolGray,
+      gray: colors.trueGray,
+      trueGray: colors.trueGray,
+      warmGray: colors.warmGray,
+      red: colors.rose,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.amber,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      lightBlue: colors.lightBlue,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose
+    }
   },
   variants: {
     extend: {},
