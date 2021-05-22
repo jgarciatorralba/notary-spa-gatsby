85d31339621f5b07ae9b9288ff121053afa5d593
jgarciatorralba <jgarciatorralba@gmail.com>
2021-05-22 23:26:02 +0200

    prevent scroll on open modal

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -27,6 +27,16 @@ const Navbar = () => {
     { name: contact, to: "/contacto/" },
   ]
 
+  function fixBody(open) {
+    if (open) {
+      document.body.style.position = "fixed"
+      document.body.style.overflow = "hidden"
+    } else {
+      document.body.style.position = "relative"
+      document.body.style.overflow = "visible"
+    }
+  }
+
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -34,7 +44,10 @@ const Navbar = () => {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button
+                  className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  onClick={fixBody(open)}
+                >
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
