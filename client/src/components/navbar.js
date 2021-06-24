ec56a1303d72acc11cea5df19513fa73ad54e2b7
Jorge García <jorge@omada.es>
2021-06-24 22:19:12 +0200

    re-work display of navbar items depending on breakpoints

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -40,7 +40,7 @@ const Navbar = () => {
     <nav className="sticky top-0 bg-gray-800">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div className="mobile-button absolute inset-y-0 left-0 flex items-center">
             <button
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               onClick={handleClick}
@@ -57,15 +57,15 @@ const Navbar = () => {
             <div className="flex-shrink-0 flex items-center">
               <StaticImage
                 src="../images/demo/logo/emblem.png"
-                className="block lg:hidden h-8 w-auto"
+                className="logo block lg:hidden h-8 w-auto"
                 alt=""
                 placeholder="blurred"
                 quality={100}
                 height={32}
               />
-              <p className="hidden lg:block h-8 w-auto logo-text">{title}</p>
+              <p className="logo-text hidden lg:block h-8 w-auto">{title}</p>
             </div>
-            <div className="hidden sm:block sm:ml-6">
+            <div className="navigation sm:ml-6">
               <div className="flex space-x-4">
                 {navigation.map(item => (
                   <Link
@@ -109,10 +109,10 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="sm:hidden">
+      <div className="mobile-navigation-wrapper">
         <div
           ref={mobilePanelRef}
-          className="custom-mobile-panel px-2 pt-2 pb-3 min-w-full fixed bg-gray-800 flex flex-col justify-center content-center items-center"
+          className="mobile-navigation px-2 pt-2 pb-3 min-w-full fixed bg-gray-800 flex flex-col justify-center content-center items-center"
         >
           {navigation.map(item => (
             <Link
@@ -123,7 +123,7 @@ const Navbar = () => {
                   (originalPath.includes(item.to) && item.to !== "/")
                   ? "bg-gray-900 text-white"
                   : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                "w-3/4 my-2 block px-3 py-2 rounded-full text-base text-center font-medium"
+                "my-2 block px-3 py-2 rounded-full text-base text-center font-medium"
               )}
               aria-current={
                 item.to === originalPath ||
