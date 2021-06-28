f2a186f73c0f0d2d960921c55bf23fc15a7f0f89
jgarciatorralba <jgarciatorralba@gmail.com>
2021-06-28 22:26:54 +0200

    complete responsive styles for homepage header

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -24,7 +24,9 @@ const IndexPage = () => {
     <Layout>
       <Seo defaultTitle={defaultTitle} />
       <div className="hero-wrapper">
-        <h1 className="page-main-title text-white">{header}</h1>
+        <h1 className="page-main-title text-white text-center font-bold px-3 sm:px-4 lg:px-6 py-2 sm:py-3 lg:py-4">
+          {header}
+        </h1>
 
         <StaticImage
           src="../images/demo/homepage/hero_signature.jpg"
