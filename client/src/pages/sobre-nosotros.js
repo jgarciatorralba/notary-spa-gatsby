6ff40ab7929791bf5e3d365baef8f90403857683
jgarciatorralba <jgarciatorralba@gmail.com>
2021-07-01 22:02:50 +0200

    first draft HTML structure for about page

--- client/src/pages/sobre-nosotros.js
+++ client/src/pages/sobre-nosotros.js
@@ -25,10 +25,42 @@ const AboutPage = () => {
         description={description}
       />
 
-      <h1>
-        <Trans i18nKey={header} />
-      </h1>
-      <p>{summary}</p>
+      <div className="block-intro">
+        <h2>
+          <Trans i18nKey={header} />
+        </h2>
+        <p>{summary}</p>
+      </div>
+
+      <div className="block-values">
+        {companyValues.map(value => (
+          <div className="value-item">
+            <h3>{value.valueName}</h3>
+            <p>{value.valueDescription}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="block-team">
+        {team.map(employee =>
+          employee.isNotary === "Yes" ? (
+            <div key={employee.name} className="team-member notary">
+              <h2>{employee.name}</h2>
+              <ul>
+                {employee.highlights.map(highlight => (
+                  <li>{highlight}</li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <div key={employee.name} className="team-member">
+              <h3>{employee.name}</h3>
+              <p>{employee.position}</p>
+              <a href={`mailto:${employee.contact}`}>{employee.contact}</a>
+            </div>
+          )
+        )}
+      </div>
 
       <Link to="/">Go back to the homepage</Link>
     </Layout>
