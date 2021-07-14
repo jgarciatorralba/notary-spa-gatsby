3c5df530c2547b5ff70b878ec6deffb131d2d7c1
jgarciatorralba <jgarciatorralba@gmail.com>
2021-07-14 22:05:30 +0200

    install and enable plugin gatsby-plugin-react-svg

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -70,5 +70,13 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: /images/,
+        },
+      },
+    },
   ],
 }
