18b8ecf122459ad4cd844b360b25d5d6c16c5511
jgarciatorralba <jgarciatorralba@gmail.com>
2021-07-18 21:35:52 +0200

    add extra horizontal margin on mobile

--- client/src/pages/sobre-nosotros.js
+++ client/src/pages/sobre-nosotros.js
@@ -36,7 +36,7 @@ const AboutPage = () => {
           <p className="mb-2">{summary}</p>
         </div>
 
-        <div className="block-values mb-4 flex flex-col md:flex-row justify-between">
+        <div className="block-values mb-4 mx-4 sm:mx-0 flex flex-col md:flex-row justify-between">
           {companyValues.map(value => (
             <ValueCard
               key={value.valueKey}
