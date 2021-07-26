0b310ccfaa3796896e5e97869b7bb911275e06c7
jgarciatorralba <jgarciatorralba@gmail.com>
2021-07-26 20:03:12 +0200

    replace static image by bg-image component

--- client/src/components/memberCard.js
+++ client/src/components/memberCard.js
@@ -1,40 +1,18 @@
 import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
 
 import EnvelopeIcon from "../assets/svgs/about/team/envelope.svg"
+import AdministrationImage from "./bgImages/administrationImage"
+import OfficerImage from "./bgImages/officerImage"
+import CopyistImage from "./bgImages/copyistImage"
 
 import "../styles/components/memberCard.scss"
 
 const MemberCard = ({ name, position, contact }) => {
   return (
     <div className="team-member px-4 pt-4 pb-6 text-center mx-auto my-4">
-      {contact.includes("laura") && (
-        <StaticImage
-          src="../assets/images/demo/about/team/officer.jpg"
-          alt=""
-          placeholder="blurred"
-          quality={70}
-          className="mb-2"
-        />
-      )}
-      {contact.includes("emilio") && (
-        <StaticImage
-          src="../assets/images/demo/about/team/administration.jpg"
-          alt=""
-          placeholder="blurred"
-          quality={70}
-          className="mb-2"
-        />
-      )}
-      {contact.includes("gemma") && (
-        <StaticImage
-          src="../assets/images/demo/about/team/copyist.jpg"
-          alt=""
-          placeholder="blurred"
-          quality={70}
-          className="mb-2"
-        />
-      )}
+      {contact.includes("laura") && <OfficerImage />}
+      {contact.includes("emilio") && <AdministrationImage />}
+      {contact.includes("gemma") && <CopyistImage />}
       <h3 className="text-xl">{name}</h3>
       <p className="uppercase mb-4">{position}</p>
       <a
