efa2d9509cb4807d395a911e08fc7040ef0aecdd
Jorge García <jorge@omada.es>
2021-07-29 22:18:31 +0200

    create new bgImage component for homepage

---
+++ client/src/components/bgImages/homepageImage.js
@@ -0,0 +1,32 @@
+import * as React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import { getImage } from "gatsby-plugin-image"
+import { BgImage } from "gbimage-bridge"
+
+import "../../styles/components/bgImages/homepageImage.scss"
+
+const HomepageImage = ({ children }) => {
+  const { image } = useStaticQuery(
+    graphql`
+      query {
+        image: file(relativePath: { eq: "demo/homepage/hero_signature.jpg" }) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 100
+              placeholder: BLURRED
+              formats: [AUTO, WEBP, AVIF]
+            )
+          }
+        }
+      }
+    `
+  )
+
+  const pluginImage = getImage(image)
+
+  return <BgImage image={pluginImage} className="hero-wrapper">
+    {children}
+  </BgImage>
+}
+
+export default HomepageImage
