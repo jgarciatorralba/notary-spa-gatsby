aa349f224b7e351fc11d8bc8ed6f348dcf728c07
Jorge García <jorge@omada.es>
2021-08-07 20:13:32 +0200

    add demo input elements

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -3,9 +3,30 @@ import * as React from "react"
 import "../styles/components/form.scss"
 
 const Form = () => {
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log("submit!")
+  }
+
   return (
     <div className="section-form mb-4">
-      <h2>Hello</h2>
+      <form
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+          id="grid-first-name"
+          type="text"
+          placeholder="Jane"
+        />
+
+        <button
+          className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded-full"
+          type="submit"
+        >
+          Sign Up
+        </button>
+      </form>
     </div>
   )
 }
