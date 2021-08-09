4edcf75173d32eb8a7c18cd08e220ed2aa60b367
Jorge García <jorge@omada.es>
2021-08-09 21:33:14 +0200

    import and use inputGroup and textareaGroup

--- client/src/components/form/form.js
+++ client/src/components/form/form.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 
-import Input from "./input"
+import InputGroup from "./inputGroup"
+import TextareaGroup from "./textareaGroup"
 import Button from "./button"
 
 import "../../styles/components/form/form.scss"
@@ -20,24 +21,62 @@ const Form = () => {
       <form
         onSubmit={handleSubmit}
       >
-        <Input
-          htmlType="text"
-          id="fullname"
-          name="fullname"
-          classes={classNames(
+        <div className="flex flex-wrap justify-between">
+          <InputGroup
+            wrapperClasses="w-full sm:w-5/12 mb-6"
+            labelClasses="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            labelText="First Name"
+            htmlType="text"
+            id="fullname"
+            name="fullname"
+            inputClasses={classNames(
+              "appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            )}
+            placeholder="Jane"
+            errorClasses="text-red-500 text-xs italic"
+            errorText="Please fill out this field."
+          />
+
+          <InputGroup
+            wrapperClasses="w-full sm:w-5/12 mb-6"
+            labelClasses="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            labelText="Email"
+            htmlType="email"
+            id="email"
+            name="email"
+            inputClasses={classNames(
+              "appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            )}
+            placeholder="janedoe@email.com"
+            errorClasses="text-red-500 text-xs italic"
+            errorText="Please fill out your email."
+          />
+        </div>
+
+        <TextareaGroup
+          wrapperClasses="mb-6"
+          labelClasses="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          labelText="Message"
+          id="message"
+          name="message"
+          textareaClasses={classNames(
             "appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
           )}
-          placeholder="Jane"
+          placeholder="Your message here"
+          errorClasses="text-red-500 text-xs italic"
+          errorText="Please fill out this field."
         />
 
-        <Button
-          type="submit"
-          classes={classNames(
-            "shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded-full"
-          )}
-        >
-          Sign up
-        </Button>
+        <div className="mb-6">
+          <Button
+            type="submit"
+            classes={classNames(
+              "shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded-full"
+            )}
+          >
+            Sign up
+          </Button>
+        </div>
       </form>
     </div>
   )
