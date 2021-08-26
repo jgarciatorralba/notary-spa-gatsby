acbfbc8f51c3c65081a28fab496e13e23e5c0972
jgarciatorralba <jgarciatorralba@gmail.com>
2021-08-26 21:16:27 +0200

    calculate new min height on error page

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -1,14 +1,47 @@
 import * as React from "react"
+import { useI18next } from "gatsby-plugin-react-i18next"
 import PropTypes from "prop-types"
 
 import Navbar from "./navbar"
 import Footer from "./footer"
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
+}
+
 const Layout = ({ children }) => {
+  const { originalPath } = useI18next()
+
+  const validPaths = [
+    "/sobre-nosotros/",
+    "/servicios/",
+    "/servicios/tramites-inmobiliarios/",
+    "/servicios/donaciones/",
+    "/servicios/voluntades/",
+    "/servicios/tramites-societarios/",
+    "/servicios/tramites-financieros/",
+    "/servicios/poderes/",
+    "/servicios/tramites-matrimoniales/",
+    "/contacto/",
+    "/aviso-legal/",
+    "/politica-privacidad/",
+    "/politica-cookies/",
+  ]
+
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <main
+        className={classNames(
+          originalPath === "/"
+            ? ""
+            : !validPaths.includes(originalPath)
+            ? "is-error-page"
+            : ""
+        )}
+      >
+        {children}
+      </main>
       <Footer />
     </>
   )
