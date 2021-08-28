0d8f1540e4d2d72ec2442da42bccc84d559ddf19
Jorge García <jorge@omada.es>
2021-08-28 18:21:25 +0200

    first draft page services

--- client/src/pages/servicios/index.js
+++ client/src/pages/servicios/index.js
@@ -1,19 +1,42 @@
 import * as React from "react"
-import { Link, useI18next } from "gatsby-plugin-react-i18next"
+import { Link, useTranslation } from "gatsby-plugin-react-i18next"
 import { graphql } from "gatsby"
 
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 
-const ServicesPage = () => (
-  <Layout>
-    <Seo title="Services" />
-    <h1>Services</h1>
-    <Link to="/servicios/donaciones">Donations</Link>
-    <br></br>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+import "../../styles/pages/servicios/index.scss"
+
+const ServicesPage = () => {
+  const { t } = useTranslation()
+  const servicesTranslations = t("services", {
+    returnObjects: true,
+  })
+  const metaTranslations = t("metadata", {
+    returnObjects: true,
+  })
+
+  const { title: defaultTitle, description } = metaTranslations
+  const { title, serviceList: services } = servicesTranslations
+
+  return (
+    <Layout>
+      <Seo
+        defaultTitle={defaultTitle}
+        pageTitle={title}
+        description={description}
+      />
+
+      <div className="services-wrapper max-w-7xl mx-auto px-4 md:px-6 lg:px-8 py-8">
+        {(services.map(service => (
+          <div className="service-card flex justify-center items-center p-8 m-4 border bg-red-500 text-center">
+            {service.name}
+          </div>
+        )))}
+      </div>
+    </Layout>
+  )
+}
 
 export default ServicesPage
 
