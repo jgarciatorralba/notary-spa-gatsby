4cb36b5d6dd9efdd16da2a7228d271cb3098dabc
jgarciatorralba <jgarciatorralba@gmail.com>
2021-09-01 21:38:34 +0200

    use seo component and useTranslation hook in service-details template

--- client/src/templates/service-details.js
+++ client/src/templates/service-details.js
@@ -1,22 +1,41 @@
 import * as React from "react"
+import { useTranslation } from "gatsby-plugin-react-i18next"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
+import Seo from "../components/seo"
 
 import "../styles/templates/service-details.scss"
 
 const ServiceDetails = ({ data }) => {
-  const { title, image } = data.markdownRemark.frontmatter
+  const { title: service, image } = data.markdownRemark.frontmatter
   const pluginImage = getImage(image)
+
+  const { t } = useTranslation()
+  const serviceTranslations = t(service, {
+    returnObjects: true,
+  })
+  const metaTranslations = t("metadata", {
+    returnObjects: true,
+  })
+
+  const { title: defaultTitle, description } = metaTranslations
+  const { title } = serviceTranslations
 
   return (
     <Layout>
-      <div>
-        <p>{title}</p>
-      </div>
-      <div>
+      <Seo
+        defaultTitle={defaultTitle}
+        pageTitle={title}
+        description={description}
+      />
+
+      <div className="service-details-image">
         <GatsbyImage image={pluginImage} alt="" />
+      </div>
+      <div className="service-details-wrapper max-w-7xl mx-auto px-4 md:px-6 lg:px-8 py-4 lg:py-8">
+        <p>{title}</p>
       </div>
     </Layout>
   )
