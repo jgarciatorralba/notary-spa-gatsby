4d7f48976005846cf200a61fe5c509ae2958c1d6
jgarciatorralba <jgarciatorralba@gmail.com>
2021-09-13 21:32:27 +0200

    complete design for 'privacy-policy' page

--- client/src/pages/politica-privacidad.js
+++ client/src/pages/politica-privacidad.js
@@ -17,14 +17,14 @@ const PrivacyPolicyPage = () => {
   })
 
   const { title: defaultTitle, description } = metaTranslations
-  const { title, sections } = policyTranslations
-
-  let test
-  sections.forEach(section => {
-    if (section.key === "data-protection-officer") {
-      test = section.texts[0].elements[0]
-    }
-  })
+  const { header: title, intro, sections } = policyTranslations
+  const {
+    dataTreatment,
+    purpose,
+    conservation,
+    dataProtectionOfficer,
+    userResponsibility,
+  } = sections
 
   return (
     <Layout>
@@ -36,13 +36,173 @@ const PrivacyPolicyPage = () => {
 
       <div className="privacy-policy-wrapper max-w-7xl mx-auto px-4 md:px-6 lg:px-8 py-8">
         <h2 className="mb-2 font-bold">{title}</h2>
-        <p>
-          <Trans
-            i18nKey={test}
-            components={{ 1: <a href="mailto:mail.com">contact</a> }}
-            values={{ contact: "jorge@mail.com" }}
-          />
-        </p>
+        <p className="mb-2">{intro}</p>
+
+        {dataTreatment && (
+          <>
+            <h3 className="mb-2 mt-4 font-bold">{dataTreatment.title}</h3>
+            <p className="mb-2">
+              {dataTreatment.contactDetails.companyName}
+              <br />
+              {dataTreatment.contactDetails.id.title}{" "}
+              {dataTreatment.contactDetails.id.value}
+              <br />
+              {dataTreatment.contactDetails.email.title}{" "}
+              <a href={`mailto:${dataTreatment.contactDetails.email.value}`}>
+                {dataTreatment.contactDetails.email.value}
+              </a>
+              <br />
+              {dataTreatment.contactDetails.address.title}{" "}
+              {dataTreatment.contactDetails.address.street}
+              {" - "}
+              {dataTreatment.contactDetails.address.zipCode}
+              <br />
+              {dataTreatment.contactDetails.phone.title}{" "}
+              {dataTreatment.contactDetails.phone.numbers.map(
+                (number, index) => {
+                  if (
+                    index ===
+                    dataTreatment.contactDetails.phone.numbers.length - 1
+                  ) {
+                    return number
+                  } else {
+                    return number + " | "
+                  }
+                }
+              )}
+            </p>
+          </>
+        )}
+
+        {purpose && (
+          <>
+            <h3 className="mb-2 mt-4 font-bold">{purpose.title}</h3>
+            {purpose.texts.map(text => {
+              if (text.type === "paragraph") {
+                return text.elements.map((element, index) => {
+                  return (
+                    <p key={`purpose-paragraph-${index}`} className="mb-2">
+                      {element}
+                    </p>
+                  )
+                })
+              } else if (text.type === "list") {
+                return (
+                  <ul>
+                    {text.elements.map((element, index) => {
+                      return (
+                        <li key={`purpose-list-${index}`} className="mb-2">
+                          {element}
+                        </li>
+                      )
+                    })}
+                  </ul>
+                )
+              } else {
+                return <></>
+              }
+            })}
+          </>
+        )}
+
+        {conservation && (
+          <>
+            <h3 className="mb-2 mt-4 font-bold">{conservation.title}</h3>
+            {conservation.texts.map(text => {
+              if (text.type === "paragraph") {
+                return text.elements.map((element, index) => {
+                  return (
+                    <p key={`conservation-paragraph-${index}`} className="mb-2">
+                      {element}
+                    </p>
+                  )
+                })
+              } else if (text.type === "list") {
+                return (
+                  <ul>
+                    {text.elements.map((element, index) => {
+                      return (
+                        <li key={`conservation-list-${index}`} className="mb-2">
+                          {element}
+                        </li>
+                      )
+                    })}
+                  </ul>
+                )
+              } else {
+                return <></>
+              }
+            })}
+          </>
+        )}
+
+        {dataProtectionOfficer && (
+          <>
+            <h3 className="mb-2 mt-4 font-bold">
+              {dataProtectionOfficer.title}
+            </h3>
+            {dataProtectionOfficer.texts.map(text => {
+              if (text.type === "paragraph") {
+                return text.elements.map((element, index) => {
+                  return (
+                    <p key={`dpo-paragraph-${index}`} className="mb-2">
+                      <Trans
+                        i18nKey={element}
+                        components={{
+                          1: (
+                            <a href={`mailto:${dataProtectionOfficer.contact}`}>
+                              contact
+                            </a>
+                          ),
+                        }}
+                        values={{ contact: dataProtectionOfficer.contact }}
+                      />
+                    </p>
+                  )
+                })
+              } else {
+                return <></>
+              }
+            })}
+          </>
+        )}
+
+        {userResponsibility && (
+          <>
+            <h3 className="mb-2 mt-4 font-bold">{userResponsibility.title}</h3>
+            {userResponsibility.texts.map(text => {
+              if (text.type === "paragraph") {
+                return text.elements.map((element, index) => {
+                  return (
+                    <p
+                      key={`user-responsibility-paragraph-${index}`}
+                      className="mb-2"
+                    >
+                      {element}
+                    </p>
+                  )
+                })
+              } else if (text.type === "list") {
+                return (
+                  <ul>
+                    {text.elements.map((element, index) => {
+                      return (
+                        <li
+                          key={`user-responsibility-list-${index}`}
+                          className="mb-2"
+                        >
+                          {element}
+                        </li>
+                      )
+                    })}
+                  </ul>
+                )
+              } else {
+                return <></>
+              }
+            })}
+          </>
+        )}
       </div>
     </Layout>
   )
