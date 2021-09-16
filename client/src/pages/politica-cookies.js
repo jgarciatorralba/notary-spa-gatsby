b3f8c20170a53ec479df222acbacffcb966fe2ec
jgarciatorralba <jgarciatorralba@gmail.com>
2021-09-16 21:03:58 +0200

    first draft page 'cookie-policy'

--- client/src/pages/politica-cookies.js
+++ client/src/pages/politica-cookies.js
@@ -1,17 +1,37 @@
 import * as React from "react"
-import { Link, useI18next } from "gatsby-plugin-react-i18next"
+import { useTranslation } from "gatsby-plugin-react-i18next"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const CookiePolicyPage = () => (
-  <Layout>
-    {/* <Seo pageTitle="About" /> */}
-    <h1>Cookie Policy</h1>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+import "../styles/pages/politica-cookies.scss"
+
+const CookiePolicyPage = () => {
+  const { t } = useTranslation()
+  const policyTranslations = t("cookie-policy", {
+    returnObjects: true,
+  })
+  const metaTranslations = t("metadata", {
+    returnObjects: true,
+  })
+
+  const { title: defaultTitle, description } = metaTranslations
+  const { header: title } = policyTranslations
+
+  return (
+    <Layout>
+      <Seo
+        defaultTitle={defaultTitle}
+        pageTitle={title}
+        description={description}
+      />
+      <div className="privacy-policy-wrapper max-w-7xl mx-auto px-4 md:px-6 lg:px-8 py-8">
+        <h2 className="mb-2 font-bold">{title}</h2>
+      </div>
+    </Layout>
+  )
+}
 
 export default CookiePolicyPage
 
