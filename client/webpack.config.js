21f815ab8be392771e2c2f5f37b4c772399b7842
jgarciatorralba <jgarciatorralba@gmail.com>
2021-09-18 15:21:40 +0200

    add 'definePlugin' key to webpack.config.js file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,17 @@
-module.exports = {
-  resolve: {
-    mainFields: ["main", "module"],
-  },
+const webpack = require("webpack")
+
+module.exports = (env, argv) => {
+  const mode = argv.mode || "development"
+
+  return {
+    mode,
+    resolve: {
+      mainFields: ["main", "module"],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(mode),
+      }),
+    ],
+  }
 }
