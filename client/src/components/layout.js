4230bc907c03a65cf4ee09cf8f2a7c9c85640c24
jgarciatorralba <jgarciatorralba@gmail.com>
2021-09-19 12:26:20 +0200

    import component 'cookieBanner' and use in layout

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -1,10 +1,10 @@
 import * as React from "react"
 import { useI18next } from "gatsby-plugin-react-i18next"
-import CookieConsent from "react-cookie-consent"
 import PropTypes from "prop-types"
 
 import Navbar from "./navbar"
 import Footer from "./footer"
+import CookieBanner from "./cookieBanner"
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ")
@@ -44,18 +44,7 @@ const Layout = ({ children }) => {
         {children}
       </main>
       <Footer />
-
-      <CookieConsent
-        location="bottom"
-        buttonText="Sure man!!"
-        cookieName="cookies_agreed"
-        style={{ background: "#2B373B" }}
-        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-        expires={180}
-        overlay
-      >
-        This website uses cookies to enhance the user experience.
-      </CookieConsent>
+      <CookieBanner />
     </>
   )
 }
