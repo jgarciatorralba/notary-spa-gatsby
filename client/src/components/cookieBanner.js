c529bbfecfb4bcbcf951f2c85e0181a2fe60b0ae
jgarciatorralba <jgarciatorralba@gmail.com>
2021-09-19 18:35:06 +0200

    complete design for component 'cookieBanner'

--- client/src/components/cookieBanner.js
+++ client/src/components/cookieBanner.js
@@ -1,37 +1,56 @@
 import * as React from "react"
-import { useTranslation, Link } from "gatsby-plugin-react-i18next"
+import {
+  useTranslation,
+  Trans,
+  Link,
+  useI18next,
+} from "gatsby-plugin-react-i18next"
 import CookieConsent from "react-cookie-consent"
 
 import "../styles/components/cookieBanner.scss"
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
+}
+
 const CookieBanner = () => {
+  const { originalPath } = useI18next()
+
   const { t } = useTranslation()
   const bannerTranslations = t("cookie-banner", {
     returnObjects: true,
   })
-  const { text, acceptButton, moreInfoLink } = bannerTranslations
+  const { text, acceptButton } = bannerTranslations
 
   return (
     <CookieConsent
       location="bottom"
       buttonText={acceptButton}
       cookieName="cookies_agreed"
-      style={{ background: "#2B373B" }}
-      buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
       expires={90}
-      containerClasses="cookie-banner-wrapper"
+      containerClasses={classNames(
+        originalPath !== "/" ? "not-front-page" : "",
+        "cookie-banner-wrapper"
+      )}
       buttonWrapperClasses="cookie-banner-btn-wrapper"
-      buttonClasses="cookie-banner-accept-btn"
+      buttonClasses="cookie-banner-accept-btn rounded-full"
       contentClasses="cookie-banner-content"
       overlayClasses="cookie-banner-overlay"
-      // disableStyles={true}
-      debug={true}
-      overlay
+      disableStyles={true}
     >
-      <p className="mb-2">{text}</p>
-      <Link to="/politica-cookies/" className="more-info-link">
-        {moreInfoLink}
-      </Link>
+      <p>
+        <Trans
+          components={{
+            1: (
+              <Link to="/politica-cookies/" className="more-info-link">
+                link
+              </Link>
+            ),
+          }}
+        >
+          {text}
+        </Trans>
+      </p>
     </CookieConsent>
   )
 }
